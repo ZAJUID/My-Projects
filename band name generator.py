9626640fb5e2b751614ebcc39748e86503a3f1ff
ZAJUID <zajuidcabreraduran@gmail.com>
2024-10-23 10:11:27 -0600

    Add files via upload
    In this program you can enter your name and the city where youb grow up and you'll get a band mixing your name and city

---
+++ band name generator.py
@@ -0,0 +1,4 @@
+print("*****Welcome to the band name generator*****")
+city = input("in what city have you grown up?\n")
+pet = input("what's the name of your pet?\n")
+print("the name of your band could be: " + city + " " + pet)
